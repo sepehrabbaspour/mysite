468bd905080d2bc325ab9e1f458b6f1dc21561fd
sepehr abbaspour <sepehr.abbaspour.1382@gmail.com>
2026-07-19 23:58:55 +0330

    Register models in Django admin
    Registered models using admin.site.register() to make them manageable from the Django admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from blog.models import Post #esm classi ke tooye models blog sakhtim 
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post , PostAdmin) #ba in ravesh rejister mikonim class moon (model moon) ro ke tooye panel admin betoonim model moon ro bebinim
+
+
+#migim bia va ye seri moshakhase be registration ie ke alan dari ezafe kon , az ye class vaset estefade mikonam va oon class ro be
+#onavan ye arguman pas midam
+
+#estandard khsi baraye esm gozari vojood nadare vali mishe injoori begim ke (dar inja) kodoom model ro mikham barash shakhes taien konam?
+#Post ro . che amaliati mikhaym anjam bedim ? admin ro mikhaym taghir bedim pas darim class PostAdmin().
+#az koja bayad ers bari kone? az tooye admin.ModelAdmin. hala in az koja miad in ? tooye documention django ino gofte :)
+
+#hala chejoori behesh begam eleman hayio ke
--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model): #table esm class be onvan esm table dar nazar gerefte 
     #tag
     #category
     counted_views = models.IntegerField(default=0) #(default=0) migim meghdar avaliesh 0 bashe
-    status = models.BooleanField(default=False)
+    status = models.BooleanField(default=False) #yani meghdar avalie ash false bashe
     published_date = models.DateTimeField(null=True) #ino chon khodemoon gharare tarif konim va bana nist az jayi biad khali mizarim
     created_date = models.DateTimeField(auto_now_add=True) #auto_now_add=True
     #in yanoi zaman sakhte shodan field, hamin alan bokhore zaman sakhte shodanesh
@@ -23,3 +23,17 @@ class Post(models.Model): #table esm class be onvan esm table dar nazar gerefte 
 #maghadir hatma bayad barash meghdar null ya defult barash taien konim mesl alan
 
 #amma age avalin bar hast ke darim makemigratin anjam midim hich iradi nakhahad gereft
+
+    def __str__(self): #
+        return "{} - {}".format(self.title , self.id) #inja dar vaghe darim migim ye function ezafe mikonam ke ghavaed classam ro over write konam 
+#inja object self ma dare az eleman haye marboot be modelasion miad doroste? 
+#hala migim be jaye inke biai va object haro tooye modelasion neshoom bedi bia va title haro neshon bede yani : 
+#return self.title. ba function __str__ ham ke migim bejaye object ha bia va title ha sho be man bargardoon.
+#dar vaghe title har posti ie ke marboot be khodeshe. faghat deghat kon ke tooye class bayad bashe in function va dar asl ye method hast
+#alan asami ke tooye orm mibinim asami object ha va id ha ke ghabl az in method midim nist va dare title hasho neshoon mide behemoon.
+#hala masala man age biam bagem self.content , eleman content haro namayesh mide 
+#hala agar bekhaym kenaresh shomare id haro ham namayesh bedim mitoonim az formated string estefade konim ke be in soorat haske ke:
+#return "{} - {}".format(self.title , self.id). migim ham title haro behem namayesh bede va ham id haro  
+
+
+
