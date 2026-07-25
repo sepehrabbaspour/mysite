d229721e4087f0bfdc7d52af58e4e1496109a8af
sepehr abbaspour <sepehr.abbaspour.1382@gmail.com>
2026-07-25 14:47:33 +0330

    Add dynamic post detail URL with 404 handling
    - Added dynamic URL path to retrieve posts posts by ID
    
    - Passed the post ID from the URL to the view
    
    - Retrieved the requested post from the data base Using Django ORM
    
    - Passed the post data to the template through the context
    
    - Added 404 handling for non-existent post IDs

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,21 @@ app_name = 'blog'
 urlpatterns = [
     path("" , blog_view , name='index'),
     path("single" , blog_single , name='single'),
-    path('test' , test , name='test'),
+    path('post-<int:pid>' , test , name='test'),
 ]
+
+
+#khob kari ke mikhaym anjam bedim ine ke eleman hayi ijad konim ke be soorat dinamic parameter haro az tarigh url daryaft konand
+#va tooye safhe bahemoon namayesh bedan
+#tooye path ie ke test ro neveshtim be onvan url bayad biaim va parametr voroodi bedim
+#aval az hame migim be jaye jaygahi ke man daram test ro vared mikonam (avalin test (url)) mikham be soorat dinamic ye reshte vared konam
+#va dar nahayat ye esm behesh midim ke inja esmesho gozashtim name be in soorat <str:name>
+#yani migam az in be baad inja ye moteghayer voroodi ba esm name behet midam. hala in name ro bayad tooye view estefade kard.
+#hala mitoonim bejaye 1 parameter 2 ya chand parameter bezarim , be in soorat <str:name>/<str:family_name/<int:age>
+#pas alan fahmidim mitoonim parametr hayi ke lazem darim ro az tarigh url ham be view ha bedim
+#masala hata mishe behesh style dad be in soorat <str:name>/lastname/<str:family_name>/age/<int:age>
+#va url ham bayad be hamin soorat bashe : http://127.0.0.1:8000/blog/sepehr/lastname/abbaspour/age/22
+#chon alan sakhtar jadid barash tarahi kardam. pas mishe ye jahayi ham beinesh static darnazar gereft mesl inja 
+
+#pid ke esm moteghayer mast eshare mikone be inja eshare mikone be hamoon id hayi ke be ezaye eleman hamoon tooye data base dashtim.
+ 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from blog.models import Post #table post ro inja import mikonim
+#from django.shortcuts import get_object_or_404 : in chizi ke import kardim mikhaym kari konim ke vaghti ke safhe ie peida nemishe 
+#be jaye error , khataye 404 begirim :) vali khob niazi be tarif dobarash nist va chon django.shortcuts ro bala darim 
+#faghat get_object_or_404 ro import mikonim 
+#hala query variable post paien ro berim be shekli benevisim ke aval biad barresi kone , age vojood dasht namayesh bede , 
+#agar vojood nadasht , be jaye error , 404 behemoon bargardoone
+
 
 def blog_view(request):
     posts = Post.objects.filter(status=1)
@@ -10,10 +16,25 @@ def blog_single(request):
     context = {'title' : 'bitcoin crashed again!' , 'content' : 'bitcoin was flying but now grounded as always' , 'author' : 'Sepehr Abbaspour'}
     return render(request , 'blog/blog-single.html' , context)
 
-def test(request):
-    posts = Post.objects.all()
-    context = {'posts':posts}
+def test(request , pid): #migim gharare baraye to ye motagher voroodi be esm name biad
+    #posts = Post.objects.all()
+    post = get_object_or_404(Post , pk=pid)
+    #inja migim ke bro barresi kon , agar dakhel model post man shakhesi ba filteri ke alan behet dadam (pk ke hamoon primery key hast)
+    #(mitoonim ba id ham benevisim hich farghi nadare) vojood dare ya na ke primery key ham barabar hamoon pid ke beheah dadim bashe
+    #vaghti query ejra mishe agar safhe vojood dasht neshoon bede , agar safhe vojood nadasht , bejaye error 404 bede.
+    #yani shakhes aval man tooye get_object_or_404 modelam hast , shakhes dovom fiteri ke gharare roosh emal beshe
+
+    #post = Post.objects.get(id=pid) #yani migim boro az tooye post va object hash ooni ro peida kon ke id sh barabar pid voroodi mane
+    #id khob tooye admin har post ye id dasht dige , va pid ham oon id hast ke man daram tooye safhe am az url migiram
+    #yani migam boro oon posti ke id sh ba pid safhe man yeki bood ro biar va berizesh tooye variable post
+    #hala age vojood dasht post ro baraye man bargardoon be soorat line ie ke context ro toosh neveshtim baadi 
+    #yadet nare tooye safhe html ham pasesh bedi object ro ba jinja
+    #agar vojood nadashte bashe khata darim , mige in query ke zadi vojood nadare 
+    #age vojood dashte bashe ba tavajoh be chizi ke tooye test.html pas dadim behemoon barmigarde
+    context = {'post':post} #inja migim age name oomad bia va tooye safhe behem erja esh bede
     return render(request , 'test.html' , context)
+
+#vakeshi kardan etelaat az data base
 
 #ye masale ie baraye filter kardan shoon masala begam oonayi ke status shoon 1 hast yani goftam ke montasher beshan.
 #osooli tarin rah in kar tooye views.py be soorat zir anjam beshe : posts = Post.objects.filter(status=1)
@@ -21,4 +42,11 @@ def test(request):
 #dar in soorat ke filter bokonim dige hamaro behem neshoon nemide va faghat ooni ro neshoon mide ke montasher shode
 #age status 0 bashe oonayi ro neshoon mide ke status shoon barabar 0 hast ya hanoon montasher nashodan.
 #vali alan baraye dark behtar if dar jinja ino all mizarim ta ba jinja shart benevisim.
+
+
+
+#dynamic kardan url ha : 
+
+#har chand ta shakhesi ke tooye urls tarif karde bashim inja bayad neveshte bashan va tooye context ham neveshte beshan
+#masala dar inja seta parametr bayad be url bedim ta kar kone : 2 ta string , ye doone int
 
