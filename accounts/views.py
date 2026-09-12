291633a9e2f5ef69fb1df148e7846bc584d3dde4
Sepehr <sepehr.abbaspour.1382@gmail.com>
2026-09-12 23:59:47 +0330

    Handling user logins in views and templates
    Checking user login in the accounts app in view.py file (commented)
    
    Checking user login in the template (login.html) (commented out)

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 def login_view(request):
+    # if request.user.is_authenticated: #inja ye attribute az request darim ke be ma mige aya karbar ma login karde ya na. parantez nadare !!
+    #     message = f'user is authenticated as {request.user.username}'
+    #     #inja ba formated string behesh migim ke az tooye request karbar man username user ro dar biar va benevis ke che user ie login karde
+    # else:
+    #     message = 'user is not authenticated'  
+
+    # return render(request , 'accounts/login.html' , {'message' : message})
+
     return render(request , 'accounts/login.html')
+    #deghat kon ke alan ma tooye view in barresi kardim ke karbar ma login hast ya na (comment shode) va message haro . be soorat
+    #key value pas dadim tooye safhe va tooye template ham ba key mesage be soorat {{message}} darj sh kardim
+    #hala berim hamin kar ro tooye templte anjam bedim
 
 def logout_view(request):
     # return render(request , 'accounts/logout.html')
