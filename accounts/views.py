e4c205b25656d2223b466677d19460ee458cc38f
Sepehr <sepehr.abbaspour.1382@gmail.com>
2026-09-15 17:32:15 +0330

    Use Django authentication form for login
    - Added Django's built-in AuthenticationForm to handle login validation.
    - Added authentication state checks and form-based login handling.
    - Updated the login template to render the authentication form dynamically.
    - Added initial signup and logout views.

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render , redirect #baraye redirect be ye safhe dige
 from django.contrib.auth import authenticate, login 
+from django.contrib.auth.forms import AuthenticationForm #baraye estefade az form haye bulit-in django
 # Create your views here.
 
 def login_view(request):
-    if request.method == 'POST': #agar request post barat oomad etelaatesh ro daryaft kon
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password) #in user ie ke dare miad asa vojood dare ya na , majule authenticate
-        #be hamin manzoor import shode
-        if user is not None: #agar user khali nabood (vojood dasht) nokte : in karo nakonim be in soorate ke mige in user vojood nadare vali rafti baraye login , exception mikhore
-            login(request , user) #be ezaye requesti ke ferestade va etelaat karbar (user) agar dorost bood login sh mikonim , function login ro ham bala import kardim
-            return redirect('/') #baraye redirect be ye safhe dige dar soorat login movafagh / inja redirect mishe be safhe asli
-        
-    return render(request , 'accounts/login.html') #agar movafaghiat amiz nabood bargarbe be hamin safhe , va mitoone ye seri peygham ha bede
+    if not request.user.is_authenticated: #agar user login nakarde bood inaro behesh neshoon bede , tooye else migim agar login karde bood , bargard be safhe asli
+        if request.method == 'POST': #agar request post barat oomad etelaatesh ro daryaft kon
+            form = AuthenticationForm(request=request , data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password) #in user ie ke dare miad asa vojood dare ya na , majule authenticate
+                #be hamin manzoor import shode
+                if user is not None: #agar user khali nabood (vojood dasht) nokte : in karo nakonim be in soorate ke mige in user vojood nadare vali rafti baraye login , exception mikhore
+                    login(request , user) #be ezaye requesti ke ferestade va etelaat karbar (user) agar dorost bood login sh mikonim , function login ro ham bala import kardim
+                    return redirect('/') #baraye redirect be ye safhe dige dar soorat login movafagh / inja redirect mishe be safhe asli
+
+        form = AuthenticationForm()
+        context = {'form' : form}
+        return render(request , 'accounts/login.html' , context) #agar movafaghiat amiz nabood bargarbe be hamin safhe , va mitoone ye seri peygham ha bede
+
+    else:
+            return redirect('/')
 
 
 
